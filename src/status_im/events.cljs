bf02714897caac27579dc3f273462581a909dd5b
decanus <dean.eigenmann@icloud.com>
2019-03-15 16:56:55 +0100

    Revert "[#7527] Extension is not shown in chat if Development mode is switched off"
    This reverts commit 38aca129cd65216078381d10abb7fb7347fce427.

--- src/status_im/events.cljs
+++ src/status_im/events.cljs
@@ -594,7 +594,7 @@
 (handlers/register-handler-fx
  :extensions.ui/install-extension-button-pressed
  (fn [cofx [_ url]]
-   (extensions.registry/install-from-message cofx url true)))
+   (extensions.registry/load cofx url true)))
 
 (handlers/register-handler-fx
  :extensions.ui/install-button-pressed
